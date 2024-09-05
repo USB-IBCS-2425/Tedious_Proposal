8052137f96a3de821b2a531e7818872ee1444420
Zheyun Zhang <zheyunzhang@Zheyuns-MacBook-Pro.local>
2024-09-05 14:04:43 -0700

    uploading notes

---
+++ notes9_5.java
@@ -0,0 +1,54 @@
+class notes9_5 {
+
+	public static void main(String[] args) {
+		
+		//Data types
+		//defined an integer variable with a value of 4
+
+		int x = 5;
+
+		// redefine the variable. set it equal to 6
+
+		x = 6; 
+
+		// cast different types onto existing data
+
+		float y = (float) x;
+
+		System.out.println(y);
+
+		//strings are non-primitive, a sum of characters
+		String favcolor = "red";
+		System.out.println(favcolor);
+
+		//Strings can be add together 
+		//Datatype doesnt matter for add
+
+		System.out.println(favcolor + " " + x);
+
+		//Operators built in to java
+		// + , - , * , / , % , ++, --
+
+		int z = 10;
+		z = z++;
+
+		// z is currently = 12
+
+		z = z % 2;
+		
+
+
+
+
+
+
+
+
+	}
+
+
+
+
+
+
+}
