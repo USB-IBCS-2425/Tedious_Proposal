20f6284d9450c0d1d71c1c1eb9132040d0d59ea9
Zheyun Zhang <zheyunzhang@Zheyuns-MacBook-Pro.local>
2024-09-08 18:47:04 -0700

    uploading notes

---
+++ Pset2.java
@@ -0,0 +1,51 @@
+class Pset2 {
+	public static void main(String[] args) {
+		String name = "Zion";
+		String date = "Sep 8th";
+		System.out.println(name);
+		System.out.print(date);
+		System.out.println(" ");
+		System.out.println(" ");
+		int number_students:12;
+		System.out.println(number_students + " students are in our class");
+		System.out.println("A new student is enrolled in the class!");
+		number_students += 1;
+		System.out.println(number_students + " students are in our class");
+
+		if (number_students >4){
+			System.out.println("class is canceled");
+
+		} else {
+			System.out.println("class will continue");
+
+		}
+		int number_characters = 4;
+		number_students - number_characters;
+		if (number_students >4){
+			System.out.println("class is canceled");
+
+		} else {
+			System.out.println("class will continue");
+		}
+		boolean 3equal = (number_students % 3 = 0);
+		if (3equal) {
+			System.out.println(3equal + (number_students/3));
+
+		} else {
+			int need = 3 - (number_students % 3);
+			System.out.println(need);
+		}
+
+
+
+
+
+
+
+
+		
+	}
+
+
+
+}
